b53f6ff679a1424fb73aebc1e8f7c94e007520b7
Solar5503 <spidervvv@mail.ru>
2023-01-04 00:02:23 +0300

    add action creators

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,9 +1,27 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useActions } from '../hooks/useActions';
 import { useTypedSelector } from '../hooks/useTypedSelector';
+import { fetchUsers } from '../store/action-creators/user';
 
 const UserList = () => {
   const { users, loading, error } = useTypedSelector((state) => state.user);
+  const { fetchUsers } = useActions();
 
-  return <div>UserList</div>;
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  if (loading) return <h1>Идет загрузка...</h1>;
+  if (error) return <h1>{error}</h1>;
+
+  return (
+    <div>
+      {users.map((user) => (
+        <div key={user.id}>{user.name}</div>
+      ))}
+    </div>
+  );
 };
 
 export default UserList;
