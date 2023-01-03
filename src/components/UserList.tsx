5ea33eba7ff06e874a055864ee5bfaed750593ab
Solar5503 <spidervvv@mail.ru>
2023-01-04 01:38:26 +0300

    add todos

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,8 +1,6 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import { useActions } from '../hooks/useActions';
 import { useTypedSelector } from '../hooks/useTypedSelector';
-import { fetchUsers } from '../store/action-creators/user';
 
 const UserList = () => {
   const { users, loading, error } = useTypedSelector((state) => state.user);
